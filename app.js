60318ca421f59d3cf75e783f038d12cff660d604
Arun-goog <63851187+Arun-goog@users.noreply.github.com>
2022-07-12 18:14:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+var num = Math.floor(Math.random() * 4) + 1;
+
+$("#card1").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 1.jpeg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
+
+$("#card2").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 2.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
+$("#card3").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 3.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
+
+$("#card4").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 4.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
+$("#card5").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 5.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
+
+$("#card6").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 6.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
+$("#card7").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 7.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
+
+$("#card8").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card 8.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
